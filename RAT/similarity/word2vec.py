03d3d654950ac02344fd6db7dc72bbb3a7ba04b2
NightThunder <night.thunderx@gmail.com>
2019-04-28 22:22:45 +0200

    now using text_preprocessing.py

--- RAT/similarity/word2vec.py
+++ RAT/similarity/word2vec.py
@@ -1,6 +1,5 @@
-from RAT.pushshift.get_json_data import load_posts, get_post_list
+from RAT.similarity.text_preprocessing import clean_posts
 import gensim
-import os
 import logging
 import multiprocessing
 
@@ -8,28 +7,14 @@ import multiprocessing
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
-def read_input(file_name):
-    file = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../pushshift/' + file_name)
-    load = load_posts(file)
-    return get_post_list(load)
-
-
-def sentences(data):
-    tokenized = []
-    for title in data:
-        tokenized.append(gensim.utils.simple_preprocess(title.title))
-
-    return tokenized
-
-
-file_name = ''
-post_data = sentences(read_input(file_name))
+file_name = '.json'
+post_data = clean_posts(file_name)
 
 cores = multiprocessing.cpu_count()
 
 model = gensim.models.Word2Vec(size=300, window=2, min_count=2, workers=cores-1)
 model.build_vocab(post_data)
-model.train(post_data, total_examples=len(post_data), epochs=5)
+model.train(post_data, total_examples=len(post_data), epochs=10)
 
 
 # print(model.wv.similarity('', ''))

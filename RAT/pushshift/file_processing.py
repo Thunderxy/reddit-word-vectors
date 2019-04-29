2b80dba145fcf1fd8291bb7afcd98e072d9b83c5
NightThunder <night.thunderx@gmail.com>
2019-04-29 21:59:35 +0200

    added json_as_obj_lst, using gzip now

--- RAT/pushshift/file_processing.py
+++ RAT/pushshift/file_processing.py
@@ -1,11 +1,16 @@
 import json
+import gzip
+import os
 import pandas as pd
 from RAT.pushshift.get_data import Post, from_timestamp
 
 
 def load_posts(file_name):
-    with open(file_name) as f:
-        data = json.load(f)
+    with gzip.GzipFile(file_name) as f:
+        json_bytes = f.read()
+
+    json_str = json_bytes.decode('utf-8')
+    data = json.loads(json_str)
 
     return data
 
@@ -25,6 +30,12 @@ def get_post_list(data):
 
     return post_object_lst
 
+
+def json_as_obj_lst(file_name):
+    """Converts json to list of Post objects."""
+    file = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../pushshift/' + file_name)
+    load = load_posts(file)
+    return get_post_list(load)
 
 # posts = load_posts('')
 # posts_lst = get_post_list(posts)

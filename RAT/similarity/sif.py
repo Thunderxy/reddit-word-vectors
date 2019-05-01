2b54f816d9b34cdbb2b1b0a5ca9df92106a94d51
NightThunder <night.thunderx@gmail.com>
2019-05-01 22:00:15 +0200

    todo

---
+++ RAT/similarity/sif.py
@@ -0,0 +1,5 @@
+# smooth inverese frequency
+# https://stackoverflow.com/questions/22129943/how-to-calculate-the-sentence-similarity-using-word2vec-model-of-gensim-with-pyt
+# https://github.com/PrincetonML/SIF
+# https://github.com/peter3125/sentence2vec/blob/master/sentence2vec.py
+# TODO: make this thing

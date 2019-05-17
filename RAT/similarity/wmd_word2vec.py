d23e7f896bf64fc759b67df03b0d04d81e22083a
NightThunder <night.thunderx@gmail.com>
2019-05-17 21:23:10 +0200

    very slow, same results as addition

---
+++ RAT/similarity/wmd_word2vec.py
@@ -0,0 +1,34 @@
+# https://markroxor.github.io/gensim/static/notebooks/WM
+
+from gensim.similarities import WmdSimilarity
+from RAT.similarity.word2vec import load_model
+from RAT.similarity.text_preprocessing import clean_text
+from RAT.pushshift.get_data import fPosts
+from RAT.similarity.text_preprocessing import unpickle_this
+import time
+
+my_file = fPosts('')
+Posts = my_file.get_posts_list(my_file.load_posts())
+
+my_model = load_model('')
+
+my_model.init_sims(replace=True)
+
+# s1 = clean_text('')
+# s2 = clean_text('')
+# distance = my_model.wmdistance(s1, s2)
+
+post_data = unpickle_this('')
+
+instance = WmdSimilarity(post_data, my_model, num_best=10)
+
+query = clean_text('')
+
+start = time.time()
+sims = instance[query]
+print('t={} s\n'.format(time.time() - start))
+
+
+for i in range(10):
+    print('sim = {}'.format(sims[i][1]))
+    print('post: {}\n'.format(Posts[sims[i][0]].title))

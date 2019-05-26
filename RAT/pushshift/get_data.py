eb649fcb7da6756dc8ca1ff03d3a8fe971d63b97
NightThunder <night.thunderx@gmail.com>
2019-05-26 17:13:58 +0200

    classes now in classes.py, made generator for posts

--- RAT/pushshift/get_data.py
+++ RAT/pushshift/get_data.py
@@ -2,152 +2,147 @@ import requests
 import json
 import gzip
 import os
-from datetime import datetime
 import pandas as pd
 import time
+from RAT.pushshift.classes import Posts, Post, from_timestamp
 
 
-class Posts:
-    """Makes Posts object from pushshift link."""
+s = requests.Session()
 
-    s = requests.Session()
 
-    def __init__(self, n=int(1e12), query='', sort='desc', size=25, after='', before=int(time.time()), sub=''):
-        self.n = n
-        self.query = query
-        self.sort = sort
-        self.size = size
-        self.after = after
-        self.before = before
-        self.sub = sub
+def get_from_pushshift(url):
+    """Pulls data from pushshift API.
 
-    def make_url(self):
-        url = 'https://api.pushshift.io/reddit/search/submission/?q={}&size={}&sort={}&after={}&before={}&subreddit={}'.format(self.query, self.size, self.sort, self.after, self.before, self.sub)
-        return url
+    Args:
+        url: Address for accessing API. From Posts.make_url().
 
-    @staticmethod
-    def get_from_pushshift(url):
-        html = Posts.s.get(url)
-        data = json.loads(html.text)    # {'data': [{'author': ' ', ...}, {'author': ' ', ...}, ..., n], ?: [], ...}
-        if data:
-            return data['data']
+    Returns:
+        Json data from API.
+        None if API returns [].
+    """
+    html = s.get(url)
+    data = json.loads(html.text)
+    if data:
+        return data['data']
+    else:
+        return None
+
+
+def save_posts(my_data, file_name):
+    """Saves posts from pushshift. No threading version.
+
+    Args:
+        my_data: Posts object.
+        file_name: File name
+
+    Returns:
+        Saved file and None.
+
+    """
+    data = []
+
+    for i in range(my_data.n):
+        start = time.time()
+        my_url = my_data.make_url()
+        data_ = get_from_pushshift(my_url)
+
+        if data_:
+            new_data = data + data_
+            data = new_data
+
+            my_data.before = data[-1]['created_utc']
+            print('{} - {} @ {} s'.format(i, from_timestamp(my_data.before), time.time() - start))
         else:
-            return None
+            break
 
-    def save_posts(self, file_name):
-        """Saves Posts from pushshift to .json.gz ."""
-        data = []
+    json_str = json.dumps(data)
+    json_bytes = json_str.encode('utf-8')
 
-        for i in range(self.n):
-            start = time.time()
-            my_url = self.make_url()
-            data_ = Posts.get_from_pushshift(my_url)
+    file_name_ = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/' + file_name)
+    with gzip.GzipFile(file_name_, 'w+') as f:
+        f.write(json_bytes)
 
-            if data_:
-                new_data = data + data_
-                data = new_data
+    print('created: {}'.format(file_name))
 
-                self.before = data[-1]['created_utc']
-                print('{} - {} @ {} s'.format(i, from_timestamp(self.before), time.time() - start))
-            else:
-                break
 
-        json_str = json.dumps(data)
-        json_bytes = json_str.encode('utf-8')
+def get_DataFrame(my_data):
+    """Makes Pandas DataFrame from posts.
 
-        file_name_ = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/' + file_name)
-        with gzip.GzipFile(file_name_, 'w+') as f:
-            f.write(json_bytes)
+    Args:
+        my_data: Posts object
 
-        print('created: {}'.format(file_name))
+    Returns:
+        Posts DataFrame.
 
-    def get_DataFrame(self):
-        """Makes pd.DataFrame from Posts (works live)."""
+    """
 
-        posts = pd.DataFrame(columns=['title', 'id', 'time'])
+    posts = pd.DataFrame(columns=['title', 'id', 'time'])
 
-        for i in range(self.n):
-            my_url = self.make_url()
-            data = Posts.get_from_pushshift(my_url)
+    for i in range(my_data.n):
+        my_url = my_data.make_url()
+        data = get_from_pushshift(my_url)
 
-            if data:
-                self.before = data[-1]['created_utc']
-                new_posts = pd.DataFrame({'title': post['title'], 'id': post['id'],
-                                          'time': from_timestamp(post['created_utc'])} for post in data)
-                posts = pd.concat([posts, new_posts], ignore_index=True, sort=False)
-            else:
-                break
+        if data:
+            my_data.before = data[-1]['created_utc']
+            new_posts = pd.DataFrame({'title': post['title'], 'id': post['id'], 'time': from_timestamp(post['created_utc'])} for post in data)
+            posts = pd.concat([posts, new_posts], ignore_index=True, sort=False)
+        else:
+            break
 
-        return posts
+    return posts
 
-    def get_post_list(self):
-        """Makes list of objects Post and saves them to list (works live)."""
+
+def get_post_list(my_data):
+    """
+
+    Args:
+        my_data:
+
+    Returns:
+
+    """
+
+    post_object_lst = []
+
+    for i in range(my_data.n):
+        my_url = my_data.make_url()
+        data = get_from_pushshift(my_url)
+
+        if data:
+            my_data.before = data[-1]['created_utc']
+            for post in data:
+                post_object_lst.append(Post.make_post_obj(post))
+        else:
+            break
+
+    return post_object_lst
+
+
+def get_post_gen(my_data):
+    """
+
+    Args:
+        my_data:
+
+    Returns:
+
+    """
+
+    while True:
+        my_url = my_data.make_url()
+        data = get_from_pushshift(my_url)
 
         post_object_lst = []
 
-        for i in range(self.n):
-            my_url = self.make_url()
-            data = Posts.get_from_pushshift(my_url)
+        if data:
+            my_data.before = data[-1]['created_utc']
+            for post in data:
+                post_object_lst.append(Post.make_post_obj(post))
+        else:
+            break
 
-            if data:
-                self.before = data[-1]['created_utc']
-                for post in data:
-                    post_object_lst.append(Post.make_post_obj(post))
-            else:
-                break
-
-        return post_object_lst
+        yield post_object_lst
 
 
-class Post:
-    """Makes Post from dict (json)."""
-
-    def __init__(self, author, created_utc, post_id, num_comments, score, subreddit, title):
-        self.author = author
-        self.created_utc = created_utc
-        self.post_id = post_id
-        self.num_comments = num_comments
-        self.score = score
-        self.subreddit = subreddit
-        self.title = title
-
-    @staticmethod
-    def make_post_obj(post):
-        return Post(post['author'], post['created_utc'], post['id'], post['num_comments'], post['score'], post['subreddit'], post['title'])
-
-
-class fPosts:
-    """Loading saved file."""
-
-    def __init__(self, file_name):
-        self.file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/' + file_name)
-
-    def load_posts(self):
-        with gzip.GzipFile(self.file_name) as f:
-            json_bytes = f.read()
-
-        json_str = json_bytes.decode('utf-8')
-        data = json.loads(json_str)
-
-        return data
-
-    @staticmethod
-    def get_DataFrame(data):
-        posts = pd.DataFrame({'title': post['title'], 'id': post['id'],
-                              'time': from_timestamp(post['created_utc'])} for post in data)
-
-        return posts
-
-    @staticmethod
-    def get_posts_list(data):
-        post_object_lst = []
-
-        for post in data:
-            post_object_lst.append(Post.make_post_obj(post))
-
-        return post_object_lst
-
-
-def from_timestamp(unix_time):
-    """unix time -> utc time"""
-    return datetime.fromtimestamp(int(unix_time))
+r_data = Posts(size=10, subreddit='askreddit')
+x = get_post_gen(r_data)

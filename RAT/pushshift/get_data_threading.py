85fb9e058adab23e2e41c421d5c44b5bb75be6ec
NightThunder <night.thunderx@gmail.com>
2019-12-25 18:13:21 +0100

    changed global and logging

--- RAT/pushshift/get_data_threading.py
+++ RAT/pushshift/get_data_threading.py
@@ -7,61 +7,55 @@ import threading
 import logging
 from RAT.pushshift.classes import Posts, Comments, timestamp_to_utc
 
-s = requests.Session()
-lock = threading.Lock()
-api_calls = 0
-global_time = time.time()
-
 
 class GetContent:
 
-    def __init__(self, content, thread_num=1, max_per_sec=1, make_log=False):
+    def __init__(self, after, subreddit, content, before=time.time(), size=1000, thread_num=1, max_per_sec=1, log_level='warning'):
+        """
+        Parameters
+        ----------
+        thread_num: int
+            Number of threads to be used.
+        max_per_sec: float
+            API call limit.
+        log_level: None or DEBUG, INFO, WARNING
+
+        Other Parameters
+        ----------------
+        data
+        s
+        lock
+        api_calls
+        global_time
+
+        """
+        self.after = after
+        self.before = before
         self.content = content
+        self.subreddit = subreddit
+
+        if size > 1000:
+            raise ValueError('max size is 1000.')
+        else:
+            self.size = size
+
         self.thread_num = thread_num
         self.max_per_sec = max_per_sec
 
-        if make_log:
-            self.thread_log = make_log.setup_logger()
-        else:
-            self.thread_log = logging.getLogger()
-            self.thread_log.disabled = True
+        self.data = []
+        self.s = requests.Session()
+        self.lock = threading.Lock()
+        self.api_calls = 0
+        self.global_time = time.time()
 
-    def get_content(self):
-        """Thread initialization function."""
-
-        global data
-        data = []
-
-        start_time = time.time()
-        intervals = self.get_intervals()
-        threads_lst = []
-
-        for i in range(self.thread_num):
-            if self.content.is_post:
-                my_data_ = Posts(after=intervals[i][0], before=intervals[i][1], size=self.content.size,
-                                 subreddit=self.content.sub, sort=self.content.sort)
-            else:
-                my_data_ = Comments(after=intervals[i][0], before=intervals[i][1], size=self.content.size,
-                                    subreddit=self.content.sub, sort=self.content.sort)
-
-            t = threading.Thread(target=self.thread_content, name='thread{}'.format(i), args=(my_data_, 'thread{}'.format(i)))
-            time.sleep(1 / self.max_per_sec)
-            t.start()
-            threads_lst.append(t)
-            print('{} has started'.format(t.name))
-
-        for t in threads_lst:
-            t.join()
-
-        print('total time: {} s\ntotal api calls: {}'.format(time.time() - start_time, api_calls))
-        print('number of posts/comments collected: {}'.format(len(data)))
-        return data
+        if log_level:
+            logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=log_level.upper())
 
     def get_intervals(self):
-        """Make time intervals."""
+        """ Make time intervals. """
 
-        t1 = int(self.content.after)
-        t2 = int(self.content.before)
+        t1 = int(self.after)
+        t2 = int(self.before)
         delta_t = int((t2 - t1) / self.thread_num)
 
         intervals = []
@@ -70,15 +64,56 @@ class GetContent:
 
         return intervals
 
+    def get_content(self):
+        """ Thread initialization function. """
+
+        start_time = time.time()
+        intervals = self.get_intervals()
+        threads_lst = []
+
+        for i in range(self.thread_num):
+            if self.content == 'post':
+                my_data = Posts(after=intervals[i][0], before=intervals[i][1], size=self.size, subreddit=self.subreddit)
+            elif self.content == 'comment':
+                my_data = Comments(after=intervals[i][0], before=intervals[i][1], size=self.size, subreddit=self.subreddit)
+            else:
+                raise NameError
+
+            t = threading.Thread(target=self.thread_content, name='thread_{}'.format(i), args=(my_data, 'thread_{}'.format(i)))
+            time.sleep(1 / self.max_per_sec)
+            t.start()
+            threads_lst.append(t)
+            logging.info('{} has started\n'.format(t.name))
+
+        for t in threads_lst:
+            t.join()
+
+        logging.info('total time: {:.2f} s\n'.format(time.time() - start_time))
+        logging.info('total api calls: {}\n'.format(self.api_calls))
+        logging.info('number of posts/comments collected: {}\n'.format(len(self.data)))
+        time.sleep(1)
+
+        return self
+
     def thread_content(self, iter_content, thread_name):
-        """Main function for threading."""
+        """ Main function for threading.
 
-        global data
-        global api_calls
+        Parameters
+        ----------
+        iter_content: class
+            Helper class. Remembers after, before and has make_url method.
+        thread_name: str
+            Name of thread running this method.
 
-        n = self.content.n
-        for i in range(n):
-            self.throttler()
+        Returns
+        -------
+        None when done.
+
+        """
+
+        c = 0
+        while True:
+            self.throttler(thread_name)
 
             start_time = time.time()
 
@@ -86,30 +121,37 @@ class GetContent:
             data_ = self.get_from_pushshift(my_url, thread_name)
 
             if data_:
-                with lock:
-                    data += data_
-                    api_calls += 1
+                with self.lock:
+                    self.data += data_
+                    self.api_calls += 1
 
                 iter_content.before = data_[-1]['created_utc']
-                self.thread_log.info('{} on cycle: {} | runtime: {:.2f} s | after: {} | before: {}'.format(thread_name, i, time.time() - start_time, timestamp_to_utc(iter_content.after), timestamp_to_utc(iter_content.before)))
+
+                logging.debug('{} on cycle: {} | runtime: {:.2f} s | after: {} | before: {}\n'.format(thread_name, c,
+                              time.time() - start_time, timestamp_to_utc(iter_content.after),
+                              timestamp_to_utc(iter_content.before)))
             else:
                 break
 
-        self.thread_log.info('{} done'.format(thread_name))
+            c += 1
+
+        logging.info('{} done\n'.format(thread_name))
+
+        return None
 
     def get_from_pushshift(self, url, thread_name):
-        """Pulls data from pushshift API."""
+        """ Pulls data from pushshift API. """
 
-        get_from = s.get(url)
+        get_from = self.s.get(url)
 
         while get_from.status_code != 200:
-            self.thread_log.debug('to many requests sleeping {}'.format(thread_name))
+            logging.warning('to many requests sleeping {}\n'.format(thread_name))
             time.sleep(5)
 
             try:
-                get_from = s.get(url)
+                get_from = self.s.get(url)
             except requests.exceptions.RequestException as e:
-                self.thread_log.error('error in requests: {}'.format(e))
+                logging.error('error in requests: {}\n'.format(e))
                 get_from = 0
 
         ps_data = json.loads(get_from.text)
@@ -118,78 +160,52 @@ class GetContent:
         else:
             return None
 
-    def throttler(self):
-        """Throttles API connection."""
+    def throttler(self, thread_name):
+        """ Throttles API connection. """
 
-        global global_time
+        self.lock.acquire()
 
-        lock.acquire()
-
-        delta_t = time.time() - global_time
+        delta_t = time.time() - self.global_time
         limit = 1 / delta_t
         throttle_for = abs((1 / self.max_per_sec) - delta_t)
 
-        global_time = time.time()
+        self.global_time = time.time()
 
         if limit > self.max_per_sec:
-            self.thread_log.debug('over limit by {:.2f} calls/s, throttling for {:.2f} s'.format(limit - self.max_per_sec, throttle_for))
+            logging.info('{} over limit by {:.2f} calls/s, throttling for {:.2f} s\n'.format(thread_name, limit - self.max_per_sec, throttle_for))
             time.sleep(throttle_for)
-            lock.release()
+            self.lock.release()
         else:
-            lock.release()
+            self.lock.release()
 
+    def save_content(self, file_name):
+        """ Saves data to file_name.json.gz. """
 
-class LoggerConfig:
+        logging.info('saving to file...\n')
 
-    def __init__(self, name=__name__, log_file='threads.log', level=logging.DEBUG, log_format='%(asctime)s: %(levelname)s: %(message)s', print_to_console=False):
-        self.name = name
-        self.log_file = log_file
-        self.level = level
-        self.log_format = log_format
-        self.print_to_console = print_to_console
-
-    def setup_logger(self):
-
-        try:
-            os.remove(self.log_file)
-        except OSError:
-            pass
-
-        logger = logging.getLogger(self.name)
-        logger.setLevel(self.level)
-
-        formatter = logging.Formatter(self.log_format)
-
-        file_handler = logging.FileHandler(self.log_file)
-        file_handler.setFormatter(formatter)
-
-        logger.addHandler(file_handler)
-
-        if self.print_to_console:
-            logging.debug('')
-
-        return logger
-
-
-class SaveContent:
-
-    def __init__(self, file_name):
-        self.file_name = file_name
-
-    def save_content(self):
-        """Saves data to file_name.json.gz ."""
-
-        global data
-
-        logging.info('saving to file...')
-
-        json_str = json.dumps(data)
+        json_str = json.dumps(self.data)
         json_bytes = json_str.encode('utf-8')
 
-        file_name_ = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/reddit_data/' + self.file_name)
-        with gzip.GzipFile(file_name_, 'w+') as f:
+        # file_name_ = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/reddit_data/' + file_name)
+        with gzip.GzipFile(file_name, 'w+') as f:
             f.write(json_bytes)
 
-        print('created: {}'.format(self.file_name))
+        logging.info('created: {}\n'.format(file_name))
 
         return True
+
+
+if __name__ == '__main__':
+
+    def get_from_reddit(after_, before_, subreddit, size):
+
+        a = GetContent(after=after_, before=before_, subreddit=subreddit,
+                       size=size, content='comment', thread_num=2, max_per_sec=1, log_level='info').get_content()
+
+        for dct in a.data:
+            print(dct['body'])
+
+        a.save_content('my_first_save.json.gz')
+
+
+    get_from_reddit(time.time()-1000, time.time(), 'askreddit', 1000)

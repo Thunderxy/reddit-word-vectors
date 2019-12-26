2cae69b2e0c07aea79934196ba6b67e013ffc0c4
NightThunder <night.thunderx@gmail.com>
2019-12-26 13:14:21 +0100

    doc2vec input, replace links and numbers with "link" and "number"

--- RAT/text_processing/process_reddit.py
+++ RAT/text_processing/process_reddit.py
@@ -1,18 +1,17 @@
 import operator
 import re
-import pickle
-import os
 from nltk import sent_tokenize
 
 
 def remove_punctuation(sent):
+    sent_nolink = re.sub(r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+", 'link', sent)
     punct = r'!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' + '’`”“'
-    text_nopunct = ''.join(char for char in sent if char not in punct)
+    text_nopunct = ''.join(char for char in sent_nolink if char not in punct)
     return text_nopunct
 
 
 def replace_numbers(sent):
-    text_nonum = re.sub(r'[0-9]+', 'stevilka', sent)
+    text_nonum = re.sub(r'[0-9]+', 'number', sent)
     return text_nonum
 
 
@@ -20,12 +19,6 @@ def word_by_word(sent):
     tokens = re.split(r'\W+', sent[0])
     tokens_no_empty = [word.lower() for word in tokens if word != '']
     return tokens_no_empty
-
-
-def remove_stopword(tokenized_lst):
-    nltk_stopwords = ['im'] + ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'youre', 'youve', 'youll', 'youd', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'shes', 'her', 'hers', 'herself', 'it', 'its', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'thatll', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'dont', 'should', 'shouldve', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'arent', 'couldn', 'couldnt', 'didn', 'didnt', 'doesn', 'doesnt', 'hadn', 'hadnt', 'hasn', 'hasnt', 'haven', 'havent', 'isn', 'isnt', 'ma', 'mightn', 'mightnt', 'mustn', 'mustnt', 'needn', 'neednt', 'shan', 'shant', 'shouldn', 'shouldnt', 'wasn', 'wasnt', 'weren', 'werent', 'won', 'wont', 'wouldn', 'wouldnt']
-    no_stopwords = [word for word in tokenized_lst if word not in nltk_stopwords]
-    return no_stopwords
 
 
 def word2vec_input(content):
@@ -63,6 +56,18 @@ def word2vec_input(content):
     return text_in_sent
 
 
+def doc2vec_input(content):
+    w2v_input = word2vec_input(content)
+
+    doc = ''
+    for sent in w2v_input:
+        doc += ' '.join(sent) + '. '
+
+    doc = doc[:-1]
+
+    return [doc]
+
+
 def count_words(tokenized_lst, sort=None):
     word_count = {}
 
@@ -77,19 +82,3 @@ def count_words(tokenized_lst, sort=None):
         return sorted(word_count.items(), key=operator.itemgetter(1))
     else:
         return word_count
-
-
-def pickle_this(data, file_name):
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/' + file_name)
-
-    with open(path, 'wb') as f:
-        pickle.dump(data, f)
-
-
-def unpickle_this(file_name):
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/' + file_name)
-
-    with open(path, 'rb') as f:
-        data = pickle.load(f)
-
-    return data
